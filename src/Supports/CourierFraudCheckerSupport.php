1d14d7a9d1205394f267cd58334905f3355f034f
Chonchol <chonchol@alzaf.com>
2026-03-05 15:58:51 +0600

    carrybee

--- src/Supports/CourierFraudCheckerSupport.php
+++ src/Supports/CourierFraudCheckerSupport.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace Alzaf\CourierFraudCheckerBd\Supports;
 
+use Alzaf\CourierFraudCheckerBd\Services\CarryBeeService;
 use Alzaf\CourierFraudCheckerBd\Services\FreeFraudChecker;
 use Alzaf\CourierFraudCheckerBd\Services\PathaoService;
 use Alzaf\CourierFraudCheckerBd\Services\RedxService;
@@ -13,7 +13,8 @@ class CourierFraudCheckerSupport
         protected SteadfastService $steadfastService,
         protected PathaoService $pathaoService,
         protected RedxService $redxService,
-        protected FreeFraudChecker $freeFraudChecker
+        protected FreeFraudChecker $freeFraudChecker,
+        protected CarryBeeService $carryBeeService
     ) {
     }
 
@@ -24,6 +25,7 @@ class CourierFraudCheckerSupport
             'pathao'    => $this->pathaoService->pathao($phoneNumber),
             'redx'      => $this->redxService->getCustomerDeliveryStats($phoneNumber),
             'freeFraud' => $this->freeFraudChecker->freeFraud($phoneNumber),
+            // 'carrybee'  => $this->carryBeeService->getCustomerDeliveryStats($phoneNumber),
         ];
     }
 }
